b152d74f4c4283e5690a6184e149c1d22ea217b1
ISRAEL <israelss_tpc@hotmail.com>
2022-05-02 12:19:04 -0500

    inicio  del respositorio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container py-5">
+      <list-component :providers="providers"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+import axios from "axios"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import moment from 'moment'
+import ListComponent from './components/ListComponent'
+export default{  
+  data(){
+    return {
+        providers: [],
+        newProviders: []                 
+    }
+  },
+  components:{
+    ListComponent 
+  },
+  mounted(){
+    this.get()      
+  }, 
+  watch:{
+    providers(){
+      this.newProviders = this.providers.map((provider) => {        
+        if(provider.modified){
+          provider.modified = moment(provider.modified).format("MMMM DD, YYYY")
+        }
+        return provider
+      })
+    }
+  },
+  methods:{
+    async get(){
+      const token = 'cJmAc71jah17sgqi1jqaksvaksda='
+      try {
+        const resp = await axios.get('https://pnny0h3cuf.execute-api.eu-west-1.amazonaws.com/dev/providers/1',{
+                      headers: {
+                        'Authorization': `Basic ${token}` 
+                      }
+                    })
+        this.providers = resp.data        
+      } catch (error) {
+        console.log(error)
+      }
+    }    
+      
   }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "~bootstrap/scss/bootstrap";
 </style>
